7844c28b0b371f0187365f58ccf54a482f58ac51
somesimplecore <alex.itman@mail.ru>
2021-12-03 23:01:22 +0500

    Исправил замечания по PEP8 и добавил скриншоты

--- filter.py
+++ filter.py
@@ -1,16 +1,29 @@
+from line_profiler_pycharm import profile
 from PIL import Image
 import numpy as np
+
 img = Image.open(input("Введите имя исходного изображения: "))
 mosaicSize = int(input("Введите размер мозаики: "))
-grayscale = int(input("Введите желаемую градацию серого: "))
+input_grayscale = int(input("Введите желаемую градацию серого: "))
 
-def getMosaicImg(img, mosaicSize, grayscale):
-    arr = np.array(img)
+
+@profile
+def get_mosaic_img(image, mosaic_size, grayscale):
+    """
+    Возвращает мозаичную серую картинку из исходной по заданным параметрам
+    :param image: исходная картинка
+    :param mosaic_size:  размер мозайки
+    :param grayscale: градация серого
+    :return: картинка в мозаичном виде в градациях серого
+    """
+    arr = np.array(image)
     height, width = len(arr), len(arr[1])
-    for i in range(0, height, mosaicSize):
-        for j in range(0, width, mosaicSize):
-            rgbGrey = arr[i:i + mosaicSize, j:j + mosaicSize].sum() // 100 // 3 // grayscale * grayscale
-            arr[i:i + mosaicSize, j:j + mosaicSize] = rgbGrey
+    for i in range(0, height, mosaic_size):
+        for j in range(0, width, mosaic_size):
+            rgb_grey = arr[i:i + mosaic_size, j:j + mosaic_size].sum() // 100 // 3 // grayscale * grayscale
+            arr[i:i + mosaic_size, j:j + mosaic_size] = rgb_grey
     return Image.fromarray(arr)
 
-getMosaicImg(img, mosaicSize, grayscale).save(input("Введите имя результирующего изображения: "))
+
+get_mosaic_img(img, mosaicSize, input_grayscale).save(input("Введите имя результирующего изображения: "))
+help(get_mosaic_img)
